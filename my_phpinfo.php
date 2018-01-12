ce68923152aeb460aa22982ef2c24713c934791c
Oleksiy Blavat <blavat.alex@gmail.com>
2018-01-12 02:32:18 -0500

    variables

---
+++ my_phpinfo.php
@@ -0,0 +1,20 @@
+<h1>WELCOME</h1>
+<title>shit</title>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oblavat
+ * Date: 1/12/18
+ * Time: 12:31 AM
+ */
+echo("Hello" . " dipshit" . " How have you been?"); ?><br>
+<?php echo 500+9782; ?>
+
+<?php
+$item = 500;
+
+$string = "Beautiful shits.";
+echo("We have ". $item . " of " . $string);
+phpinfo();
+?>
+
