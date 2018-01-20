8b5b580bd030a85361d1186dce0c9adb5426c816
Oleksiy Blavat <blavat.alex@gmail.com>
2018-01-19 22:54:59 -0500

    refactor navigation

--- WidgetCorp/includes/functions.php
+++ WidgetCorp/includes/functions.php
@@ -30,3 +30,51 @@ function find_pages_for_subject($subject_id){
     confirm_query($page_set);
     return $page_set;
 }
+
+//2 args, current subject id, current page id
+function navigation($subject_id, $page_id) {
+    $output = "<ul class=\"subjects\">";
+    $subject_set = find_all_subjects();
+    // output query results
+    while($subject = mysqli_fetch_assoc($subject_set)) {
+        $output .="<li ";
+        if($subject["id"] == $subject_id){
+            $output .= "class=\"selected\"";
+        }
+        $output .= ">";
+
+        $output .= "<a href=\"manage_content.php?subject=";
+        $output .= urlencode($subject["id"]);
+        $output .= "\">";
+        $output .=  $subject["menu_name"];
+        $output .= "</a>";
+
+        $page_set = find_pages_for_subject($subject["id"]);
+        $output .= "<ul class=\"pages\">";
+        // output query results
+        while($page = mysqli_fetch_assoc($page_set)) {
+
+            $output .=  "<li ";
+            if($page["id"] == $page_id){
+                $output .=  "class=\"selected\"";
+            }
+            $output .=  ">";
+            $output .= "<a href=\"manage_content.php?page=";
+            $output .= urlencode($page["id"]);
+            $output .= "\">";
+            $output .= $page["menu_name"];
+            $output .= "</a>";
+            $output .= "</li>";
+        }
+        mysqli_free_result($page_set);
+        $output .= "</ul>";
+        $output .= "</li>";
+
+    }
+    //free result
+    mysqli_free_result($subject_set);
+
+    $output .= "</ul>";
+
+    return $output;
+}
--- WidgetCorp/public/manage_content.php
+++ WidgetCorp/public/manage_content.php
@@ -7,46 +7,7 @@ $selected_page_id = isset($_GET["page"]) ? $_GET["page"] : null;
 ?>
 <div id="main">
     <div id="navigation">
-        <ul class="subjects">
-            <?php $subject_set = find_all_subjects(); ?>
-            <?php // output query results
-            while($subject = mysqli_fetch_assoc($subject_set)) {
-                ?>
-                <?php
-                    echo "<li ";
-                    if($subject["id"] == $selected_subject_id){
-                        echo "class=\"selected\"";
-                    }
-                    echo ">";
-                ?>
-                    <a href="manage_content.php?subject=<?php echo urlencode($subject["id"]);?>"><?php echo $subject["menu_name"];?></a>
-                    <?php $page_set = find_pages_for_subject($subject["id"]);?>
-                    <ul class="pages">
-                        <?php // output query results
-                        while($page = mysqli_fetch_assoc($page_set)) {
-                            ?>
-                            <?php
-                            echo "<li ";
-                            if($page["id"] == $selected_page_id){
-                                echo "class=\"selected\"";
-                            }
-                            echo ">";
-                            ?>
-                                <a href="manage_content.php?page=<?php echo urlencode($page["id"]);?>"><?php echo $page["menu_name"]; ?></a>
-                            </li>
-                            <?php
-                        }
-                        ?>
-                        <?php mysqli_free_result($page_set);?>
-                    </ul>
-                </li>
-                <?php
-            }
-            ?>
-            <?php //free result
-            mysqli_free_result($subject_set);
-            ?>
-        </ul>
+        <?php echo navigation($selected_subject_id, $selected_page_id);?>
     </div>
     <div id="page">
         <h2>Manage Content</h2>
