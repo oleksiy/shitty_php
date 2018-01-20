a4a791cbf759eb427d460d2595357acf9fea31dc
Oleksiy Blavat <blavat.alex@gmail.com>
2018-01-19 20:03:50 -0500

    initial commit, also admin page

---
+++ WidgetCorp/public/admin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Widget Corp</title>
+    <link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<div id="header">
+    <h1>Widget Corp</h1>
+</div>
+<div id="main">
+    <div id="navigation">
+        &nbsp;
+    </div>
+    <div id="page">
+        <h2>Admin Menu</h2>
+        <p>Welcome to the admin area.</p>
+        <ul>
+            <li><a href="manage_content.php">Manage Website Content</a></li>
+            <li><a href="manage_admins.php">Manage Admin Users</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+    </div>
+    <div id="footer">Copyright 2018, Widget Corp</div>
+</div>
+</body>
+</html>
